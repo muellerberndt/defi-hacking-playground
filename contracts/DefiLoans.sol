698d8519a7429a1843f2914cd7f0f810702df174
Bernhard Mueller <bernhard.mueller11@gmail.com>
2020-03-02 10:26:55 +0800

    Complete DefiLoans contract

--- contracts/DefiLoans.sol
+++ contracts/DefiLoans.sol
@@ -9,35 +9,46 @@ import "./IMiniSwapExchange.sol";
 
 contract DefiLoans {
 
+    using SafeMath for uint256;
+
+    mapping(address => uint) public balances;
+    mapping(address => uint) public loans;
+
     IMiniSwapExchange exchange;
     IERC20 token;
-
-    mapping(address => uint) public vaults;
 
     constructor(address _exchange, address _token) public {
         exchange = IMiniSwapExchange(_exchange);
         token = IERC20(_token);
     }
+
+    function lockCollateral(uint256 amount) public {
+        require(token.transferFrom(msg.sender, address(this), amount));
+        balances[msg.sender] = balances[msg.sender].add(amount);
+    }
+    function withdrawCollateral(uint256 amount) public {
+        require(isCollateralSufficient(loans[msg.sender], balances[msg.sender].sub(amount)));
+        token.transfer(msg.sender, amount);
+    }
     
-    /*
+    function borrowEther(uint256 amount) public {
+        loans[msg.sender] = loans[msg.sender].add(amount);
+        require(isCollateralSufficient(loans[msg.sender], balances[msg.sender]));
+        
+        msg.sender.transfer(amount);
+    }
+
+    function repayEther() public payable {
+        loans[msg.sender] = loans[msg.sender].sub(msg.value);
+    }
+
     function minCollateral(uint256 eth_borrow) internal view returns (uint256) {
         // Minimum collateralization is 200%!
-        return _exchange.getTokenToEthOutputPrice(eth_borrow).mul(2);
+        return exchange.getTokenToEthOutputPrice(eth_borrow).mul(2);
     }    
     
-    function hasSufficientCollateral(uint256 eth_borrow, address borrower) internal view returns (bool) {
-        return (token_balances[borrower] >= minCollateral(eth_borrow));
+    function isCollateralSufficient(uint256 eth_borrow, uint256 tokens) internal view returns (bool) {
+        return (tokens >= minCollateral(eth_borrow));
     }
 
-    function depositCollateral(uint256 amount) public {
-        
-        ether_balances[msg.sender] = ether_balances[msg.sender].add(eth_borrow);
-    }
-
-    function takeLoan(uint256 eth_borrow) public {
-        require(hasEnoughCollateral(eth_borrow, msg.sender));
-        
-        ether_balances[msg.sender] = ether_balances[msg.sender].add(eth_borrow);
-    }
-    */
 }
