1b420dd4c938946f94b0554cc2bff63ee824358e
Bernhard Mueller <bernhard.mueller11@gmail.com>
2020-03-15 19:02:21 +0100

    Remove unneeded comments from migrations.js

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -4,12 +4,8 @@ const DefiLoans = artifacts.require('DefiLoans');
 
 module.exports = function(deployer) {
 
-    // Deploy the Storage contract
     deployer.deploy(JesusCoin)
-        // Wait until the storage contract is deployed
         .then(() => JesusCoin.deployed())
-        // Deploy the InfoManager contract, while passing the address of the
-        // Storage contract
         .then(() => deployer.deploy(MiniSwapExchange, JesusCoin.address))
         .then(() => deployer.deploy(DefiLoans, MiniSwapExchange.address, JesusCoin.address))
 }
