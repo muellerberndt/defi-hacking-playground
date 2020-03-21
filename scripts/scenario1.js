6807a720da0432291c5c4a96b4de87ce1b93f3a0
Bernhard Mueller <bernhard.mueller11@gmail.com>
2020-03-21 10:36:54 +0100

    Update dependencies

--- scripts/scenario1.js
+++ scripts/scenario1.js
@@ -5,6 +5,9 @@ const JesusCoin = artifacts.require('JesusCoin')
 module.exports = async function(callback) {
 
 	let token = await JesusCoin.deployed()
+
+	console.log(token);
+	
 	let factory = await MiniSwapFactory.deployed()
 
 	let result = await factory.deploy(token.address, {from: accounts[0]})
