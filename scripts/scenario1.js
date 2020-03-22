fd2fe982502563b20f9feb4f70f388e09d774aac
Bernhard Mueller <bernhard.mueller11@gmail.com>
2020-03-22 08:11:17 +0100

    Update scenario1

--- scripts/scenario1.js
+++ scripts/scenario1.js
@@ -6,17 +6,11 @@ module.exports = async function(callback) {
 
 	let token = await JesusCoin.deployed()
 
-	console.log(token);
-	
-	let factory = await MiniSwapFactory.deployed()
+	let exchange1 = await MiniSwapExchange.new(token.address)
+	let exchange2 = await MiniSwapExchange.new(token.address)
 
-	let result = await factory.deploy(token.address, {from: accounts[0]})
-
-	exchange1 = MiniSwapExchange.at(result.logs[0].args.addr)
-
-	result = await factory.deploy(token.address, {from: accounts[0]})
-
-	exchange2 = MiniSwapExchange.at(result.logs[0].args.addr)
+	await token.approve(exchange1.address, 5000000000)
+	await exchange1.addLiquidity(5000000000,5000000000, {from: accounts[0], value: 5000000000})
 
     callback();
 }
