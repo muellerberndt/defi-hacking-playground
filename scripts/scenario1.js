df40161462da95b8b25f287d1750fcea2894a002
Bernhard Mueller <bernhard.mueller11@gmail.com>
2020-03-23 06:39:20 +0100

    Update scenario script

--- scripts/scenario1.js
+++ scripts/scenario1.js
@@ -5,11 +5,25 @@ module.exports = async function(callback) {
 
 	let token = await JesusCoin.deployed()
 
-	let exchange1 = await MiniSwapExchange.new(token.address)
-	let exchange2 = await MiniSwapExchange.new(token.address)
+	// Deploy exchange 1
 
-	await token.approve(exchange1.address, 5000000000)
+	let exchange = await MiniSwapExchange.new(token.address)
+
+	await token.approve(exchange1.address, 5000000000, {from: accounts[0]})
+
 	await exchange1.addLiquidity(5000000000,5000000000, {from: accounts[0], value: 5000000000})
+	
+	console.log("Exchange at " + exchange1.address);
+
+	// Deploy exchange 2
+
+	let defiloans = await MiniSwapExchange.new(token.address)
+
+	await token.approve(exchange2.address, 5000000000, {from: accounts[0]})
+
+	await exchange2.addLiquidity(5000000000,5000000000, {from: accounts[0], value: 5000000000})
+	
+	console.log("Exchange1 at " + exchange1.address);
 
     callback();
 }
